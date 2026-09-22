905da07d93a9982084e5e89ed28ad5c03e94a316
beenzinho1003 <jinho0048@gmail.com>
2026-09-22 19:48:03 +0900

    저걸 수정하려던 게 아닌데!

--- src/main/java/com/example/springhomework/guestbook/dto/GuestbookDetailResponse.java
+++ src/main/java/com/example/springhomework/guestbook/dto/GuestbookDetailResponse.java
@@ -7,7 +7,7 @@ import java.time.LocalDateTime;
 
 @Getter
 @RequiredArgsConstructor
-public class GuestbookDetailResponse{
+public class GuestbookDetailResponse {
     private final Long id;
     private final String title;
     private final String content;
